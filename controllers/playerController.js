48185a13a5107d97f7577c3c83722636c061ce66
chouaibseridi <chouaib.94@outlook.fr>
2022-04-20 01:56:24 +0200

    Added getBestCountry() function

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,4 +1,5 @@
 const Player = require('../models/player');
+const { sequelize } = require('../models/sequelize');
 
 exports.getAllPlayers = async (req, res) => {
     await Player.findAll({
@@ -22,4 +23,40 @@ exports.getPlayerById = async (req, res) => {
     .catch(err => {
         res.send({message: err.message});
     });
+}
+
+exports.getStats = async (req, res) => {
+    const bestCountry = await getBestCountry();
+    res.send(bestCountry);
+}
+
+const getBestCountry = async () => {
+    return await Player.findAll({
+        attributes: [
+            'countryCode',
+            [sequelize.fn('GROUP_CONCAT', sequelize.literal("lastmatches SEPARATOR ''")),'lastmatches']
+        ],
+        group: 'countryCode'
+    })
+    .then(results => {
+        results.forEach(r => {
+            r.dataValues.wins = calculateWins(r.dataValues.lastmatches)
+        })
+        
+        let max = Math.max.apply(Math, results.map(function(o) { return o.dataValues.wins; }))
+        let bestCountry = results.find(obj => obj.dataValues.wins === max);
+
+        return {"bestCountry": bestCountry.dataValues.countryCode, "wins": bestCountry.dataValues.wins};
+    })
+    .catch(err => {
+        return(err.message)
+    });  
+}
+
+const calculateWins = (str) => {
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        sum += parseInt(str[i]);
+    }
+    return sum;
 }
